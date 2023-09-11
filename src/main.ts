173086d4a99a5bc28dc68fb06c1eb2f5553c0ef2
Đặng Minh Đức <ducware@gmail.com>
2023-09-11 16:39:38 +0700

    update src

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
+
   await app.listen(PORT);
 }
 bootstrap();
